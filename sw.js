33207e6d948635033c3df00d169e1dab6b7a3956
GrupoGIDca <drexlerbencomo@gmail.com>
2026-06-10 01:44:06 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+// Service Worker - GID C.A.
+const CACHE_NAME = 'gid-cache-v1';
+const urlsToCache = [
+  '/GID/',
+  '/GID/index.html'
+];
+
+// Instalar
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activar
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
+
+// Fetch - sirve desde cache si no hay internet
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
